0162628dfab0fa38dbec144233b49244dbdd7b6e
Azfar Mahmoodi <98274252+AzfarMahmoodi@users.noreply.github.com>
2024-01-02 22:27:27 +0530

    Longest alternating subsequence.cpp

---
+++ Longest alternating subsequence.cpp
@@ -0,0 +1,14 @@
+	int AlternatingaMaxLength(vector<int>&nums){
+		    // Code here
+		    int inc=1;
+		    int dec=1;
+		    for(int i=1;i<nums.size();i++){
+		        if(nums[i]>nums[i-1]){
+		            inc=dec+1;
+		        }
+		        else if(nums[i]<nums[i-1]){
+		            dec=inc+1;
+		        }
+		    }
+		    return max(inc,dec);
+		}
