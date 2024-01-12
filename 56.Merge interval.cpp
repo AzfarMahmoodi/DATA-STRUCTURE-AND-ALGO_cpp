29a1d8f165c8c575a428109d6051a35f8cd8617c
Azfar Mahmoodi <98274252+AzfarMahmoodi@users.noreply.github.com>
2024-01-12 21:37:15 +0530

    56.Merge interval.cpp

---
+++ 56.Merge interval.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        vector<vector<int>>ans;
+        sort(intervals.begin(),intervals.end());
+        ans.push_back(intervals[0]);
+        int j=0;
+        for(int i=1;i<intervals.size();i++){
+            if(ans[j][1]>=intervals[i][0]){
+                ans[j][1]=max(ans[j][1],intervals[i][1]);
+
+            }
+            else{
+                ans.push_back(intervals[i]);
+                j++;
+            }
+        }
+        return ans;
+    }
+};
