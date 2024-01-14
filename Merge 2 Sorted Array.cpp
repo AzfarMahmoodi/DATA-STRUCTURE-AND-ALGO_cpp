53cb1d25b1857be82424fab7fabfa286e287890b
Azfar Mahmoodi <98274252+AzfarMahmoodi@users.noreply.github.com>
2024-01-14 22:42:20 +0530

    Merge 2 Sorted Array.cpp

---
+++ Merge 2 Sorted Array.cpp
@@ -0,0 +1,33 @@
+vector < int > sortedArray(vector < int > arr1, vector < int > arr2) {
+    // Write your code here
+    int n=arr1.size();
+    int m=arr2.size();
+    int i = 0, j = 0; // pointers
+  vector < int > Union; // Uninon vector
+  while (i < n && j < m) {
+    if (arr1[i] <= arr2[j]) // Case 1 and 2
+    {
+      if (Union.size() == 0 || Union.back() != arr1[i])
+        Union.push_back(arr1[i]);
+      i++;
+    } else // case 3
+    {
+      if (Union.size() == 0 || Union.back() != arr2[j])
+        Union.push_back(arr2[j]);
+      j++;
+    }
+  }
+  while (i < n) // IF any element left in arr1
+  {
+    if (Union.back() != arr1[i])
+      Union.push_back(arr1[i]);
+    i++;
+  }
+  while (j < m) // If any elements left in arr2
+  {
+    if (Union.back() != arr2[j])
+      Union.push_back(arr2[j]);
+    j++;
+  }
+  return Union;
+}
