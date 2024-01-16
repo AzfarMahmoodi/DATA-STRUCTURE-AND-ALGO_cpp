42bd7776a3f01a06c4a645837b970fb721a8b51b
Azfar Mahmoodi <98274252+AzfarMahmoodi@users.noreply.github.com>
2024-01-16 21:41:37 +0530

    Zero Matrix.cpp

---
+++ Zero Matrix.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h> 
+vector<vector<int>> zeroMatrix(vector<vector<int>> &matrix, int n, int m) {
+	// Write your code here.
+	vector<int>row(n,0);
+	vector<int>col(m,0);
+	for(int i=0;i<n;i++){
+		for(int j=0;j<m;j++){
+			if(matrix[i][j]==0){
+				row[i]=1;
+				col[j]=1;
+			}
+		}
+	}
+	for(int i=0;i<n;i++){
+		for(int j=0;j<m;j++){
+			if(row[i]==1 || col[j]==1){
+				matrix[i][j]=0;
+			}
+		}
+	}
+	return matrix;
+}
