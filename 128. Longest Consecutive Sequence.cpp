437312e0e5eb6b01f5dd48d27aed245e031868b7
Azfar Mahmoodi <98274252+AzfarMahmoodi@users.noreply.github.com>
2024-01-16 21:41:02 +0530

    128. Longest Consecutive Sequence.cpp

---
+++ 128. Longest Consecutive Sequence.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int longestConsecutive(vector<int>&a) {
+           int n = a.size();
+    if (n == 0) return 0;
+
+    //sort the array:
+    sort(a.begin(), a.end());
+    int lastSmaller = INT_MIN;
+    int cnt = 0;
+    int longest = 1;
+
+    //find longest sequence:
+    for (int i = 0; i < n; i++) {
+        if (a[i] - 1 == lastSmaller) {
+            //a[i] is the next element of the
+            //current sequence.
+            cnt += 1;
+            lastSmaller = a[i];
+        }
+        else if (a[i] != lastSmaller) {
+            cnt = 1;
+            lastSmaller = a[i];
+        }
+        longest = max(longest, cnt);
+    }
+    return longest;
+    }
+};
