5704b1ac5bcf1846883b8f0e83356679f6788014
Azfar Mahmoodi <98274252+AzfarMahmoodi@users.noreply.github.com>
2024-01-19 02:07:34 +0530

    4Sum.cpp

---
+++ 4Sum.cpp
@@ -0,0 +1,33 @@
+vector<vector<int>> fourSum(vector<int>& arr, int target) {
+    // Write your code here
+    int n=arr.size();
+        vector<vector<int>> ans;
+    sort(arr.begin(),arr.end());
+    for(int i=0;i<n;i++){
+        if (i != 0 && arr[i] == arr[i - 1]) continue;
+        for(int j=i+1;j<n;j++){
+             if (j>i+1  && arr[j] == arr[j - 1]) continue;
+        
+            int k=j+1;
+            int l=n-1;
+            while(k<l){
+                int sum=arr[i]+arr[j]+arr[k]+arr[l];
+                if(sum<target){
+                    k++;
+                }
+                else if(sum>target){
+                    l--;
+                }
+                else{
+                
+                    ans.push_back({arr[i],arr[j],arr[k],arr[l]});
+                    k++;
+                    l--;
+                    while (k < l && arr[k] == arr[k - 1]) k++;
+                    while (k < l && arr[l] == arr[l + 1]) l--;
+                }
+            }
+        }
+    }
+    return ans;
+}
